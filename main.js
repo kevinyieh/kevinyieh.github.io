61e7a04c8f3182166e352561d1b355329d57fe03
kevinyieh <kevyieh@gmail.com>
2020-10-26 12:13:28 -0400

    Add basic header and nav

---
+++ main.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const burger = document.getElementById("burger");
+    const navContainer = document.getElementById("nav-container");
+    const modal = document.getElementById("nav-modal-backdrop");
+    const navContent = document.getElementById("nav-content")
+    console.log(burger)
+    burger.addEventListener("mousedown", e => {
+        modal.classList.remove("hidden");
+        navContainer.classList.add("on-screen");
+        navContent.classList.add("on-screen");
+    })
+    modal.addEventListener("mousedown", e => {
+        if(e.target === modal) {
+            modal.classList.add("hidden");
+            navContainer.classList.remove("on-screen");
+            navContent.classList.remove("on-screen");
+        }
+    })
+})
