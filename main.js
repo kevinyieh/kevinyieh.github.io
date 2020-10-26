5839be4c2cd183a054ee2d72ea09b39e241c5ca2
kevinyieh <kevyieh@gmail.com>
2020-10-26 16:55:59 -0400

    Fix nav css

--- main.js
+++ main.js
@@ -2,8 +2,14 @@ document.addEventListener("DOMContentLoaded", () => {
     const burger = document.getElementById("burger");
     const navContainer = document.getElementById("nav-container");
     const modal = document.getElementById("nav-modal-backdrop");
-    const navContent = document.getElementById("nav-content")
-    console.log(burger)
+    const navContent = document.getElementById("nav-content");
+    const close = document.getElementById("close");
+
+    close.addEventListener("mousedown", e => {
+        modal.classList.add("hidden");
+        navContainer.classList.remove("on-screen");
+        navContent.classList.remove("on-screen");
+    })
     burger.addEventListener("mousedown", e => {
         modal.classList.remove("hidden");
         navContainer.classList.add("on-screen");
