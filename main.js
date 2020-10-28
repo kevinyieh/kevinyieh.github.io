90f277bd3d445cbe6259152c30772f9ecb5f5a02
kevinyieh <kevyieh@gmail.com>
2020-10-28 14:40:41 -0400

    Add functionality to nav bar

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
+    function hideNav(){
+        modal.classList.add("hidden");
+        navContainer.classList.remove("on-screen");
+        navContent.classList.remove("on-screen");
+    }
     const burger = document.getElementById("burger");
     const navContainer = document.getElementById("nav-container");
     const modal = document.getElementById("nav-modal-backdrop");
@@ -6,9 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const close = document.getElementById("close");
 
     close.addEventListener("mousedown", e => {
-        modal.classList.add("hidden");
-        navContainer.classList.remove("on-screen");
-        navContent.classList.remove("on-screen");
+        hideNav();
     })
     burger.addEventListener("mousedown", e => {
         modal.classList.remove("hidden");
@@ -17,9 +20,35 @@ document.addEventListener("DOMContentLoaded", () => {
     })
     modal.addEventListener("mousedown", e => {
         if(e.target === modal) {
-            modal.classList.add("hidden");
-            navContainer.classList.remove("on-screen");
-            navContent.classList.remove("on-screen");
+            hideNav();
         }
     })
+
+    const home = document.getElementById("nav-home");
+    const projects = document.getElementById("nav-projects");
+    const techSkills = document.getElementById("nav-tech-skills");
+    const extendedBio = document.getElementById("nav-extended-bio");
+
+    home.addEventListener("mousedown", e => {
+        hideNav();
+        scrollToTop();
+    })
+    projects.addEventListener("mousedown", e => {
+        hideNav();
+        const projectSection = document.getElementById("projects-section");
+        projectSection.scrollIntoView({behavior: 'smooth',block: 'start'})
+    })
+    techSkills.addEventListener("mousedown", e => {
+        hideNav();
+        const technologies = document.getElementById("technologies");
+        technologies.scrollIntoView({behavior: 'smooth',block: 'start'})
+    })
 })
+
+function scrollToTop(){
+    const c = document.documentElement.scrollTop || document.body.scrollTop;
+    if (c > 0) {
+        window.requestAnimationFrame(scrollToTop);
+        window.scrollTo(0, c - c / 8);
+    }
+};
